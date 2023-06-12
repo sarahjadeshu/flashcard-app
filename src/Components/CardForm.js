005b3c111ae0c7e3a12361a3d4993938e9a0045f
Sarah Shu <65093042+sarahjadeshu@users.noreply.github.com>
2023-06-12 03:06:05 -0700

    Update CardForm.js

--- src/Components/CardForm.js
+++ src/Components/CardForm.js
@@ -1,65 +1,73 @@
 import React, { useEffect, useState } from "react";
+import { Link, useHistory, useParams } from "react-router-dom";
 
-function CardForm({ card, newCard, handleSubmit, handleChange }) {
+function CardForm({ handleSubmit, handleChange, card, newCard }) {
   if (card) {
     return (
-      <form onSubmit={handleSubmit}>
-        <h2>Edit Card</h2>
-        <div className="form-group">
-          <label>Front</label>
-          <textarea
-            id="front"
-            name="front"
-            className="form-control"
-            onChange={handleChange}
-            type="text"
-            value={card.front}
-          />
-        </div>
-        <div className="form-group">
-          <label>Back</label>
-          <textarea
-            id="back"
-            name="back"
-            className="form-control"
-            onChange={handleChange}
-            type="text"
-            value={card.back}
-          />
-        </div>
-      </form>
-    );
-  } else if (newCard) {
-    return (
-      <form onSubmit={handleSubmit}>
-        <h2>Add Card</h2>
-        <div className="form-group">
-          <label>Front</label>
-          <textarea
-            id="front"
-            name="front"
-            className="form-control"
-            onChange={handleChange}
-            type="text"
-            value={newCard.front}
-          />
-        </div>
-        <div className="form-group">
-          <label>Back</label>
-          <textarea
-            id="back"
-            name="back"
-            className="form-control"
-            onChange={handleChange}
-            type="text"
-            value={newCard.back}
-          />
-        </div>
+      <>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label>Front</label>
+            <textarea
+              id="front"
+              name="front"
+              className="form-control"
+              onChange={handleChange}
+              type="text"
+              value={card.front}
+            />
+          </div>
+          <div className="form-group">
+            <label>Back</label>
+            <textarea
+              id="back"
+              name="back"
+              className="form-control"
+              onChange={handleChange}
+              type="text"
+              value={card.back}
+            />
+          </div>
 
-        <button className="btn btn-primary mx-1" type="submit">
-          Save
+          <button className="btn btn-primary mx-1" type="submit">
+            Save
         </button>
-      </form>
+        </form>
+      </>
+    );
+  } if (newCard) {
+    return (
+      <>
+        <form onSubmit={handleSubmit}>
+          
+          <div className="form-group">
+            <label>Front</label>
+            <textarea
+              id="front"
+              name="front"
+              className="form-control"
+              onChange={handleChange}
+              type="text"
+              value={newCard.front}
+            />
+          </div>
+          <div className="form-group">
+            <label>Back</label>
+            <textarea
+              id="back"
+              name="back"
+              className="form-control"
+              onChange={handleChange}
+              type="text"
+              value={newCard.back}
+            />
+          </div>
+
+          <button className="btn btn-primary mx-1" type="submit">
+            Save
+          </button>
+        </form>
+      </>
     );
   }
 }
